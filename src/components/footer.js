41c2c5e50234e0b76af04503eb70737b09cbcb92
Gilbertkitetu <gilbertkitetu2@gmail.com>
2021-08-27 16:28:58 +0300

    ..........

---
+++ src/components/footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Footer (){
+
+    return(
+        <footer id="footer">
+        <div class="container">
+          <div class="copyright">
+            &copy; Copyright <strong><span>gilbertkitetu</span></strong>
+          </div>
+          <div class="credits">
+            Designed by gilbertkitetu AKA jsninja
+          </div>
+        </div>
+      </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import About from './main/about';
+import Contact from './main/contact';
+
+function Main () {
+    
+    return(
+<>
+            <About />
+            <Contact />
+            </>
+    );
+}
+
+export default Main;
